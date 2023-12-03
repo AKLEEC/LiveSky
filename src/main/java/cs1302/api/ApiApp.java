bba0ae9f35a103f3b9e1a50b81680b5ae0014479
AKLEEC <141100443+AKLEEC@users.noreply.github.com>
2023-12-02 22:19:00 -0500

    feat: added call to first API and extracted the JSON string

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -14,12 +14,36 @@ import javafx.scene.layout.HBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Separator;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+import java.net.http.HttpRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.IOException;
+import java.net.URI;
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+
+    /* HTTP Client */
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)
+        .followRedirects(HttpClient.Redirect.NORMAL)
+        .build();
+
+    /* Google Gson object for parsing JSON-formatted strings */
+    public static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()
+        .create();
     Stage stage;
     Scene scene;
     HBox root;
@@ -61,12 +85,16 @@ public class ApiApp extends Application {
         leftMenu.getChildren()
         .addAll(logo, directions, searchBar, searchButton);
 
+        EventHandler<ActionEvent> searchClicked = (ActionEvent e) -> {
+            this.getCoordinates("Athens");
+        };
+        searchButton.setOnAction(searchClicked);
     }
     /** {@inheritDoc} */
     @Override
     public void start(Stage stage) {
         this.stage = stage;
-
+        
         /* demonstrate how to load local asset using "file:resources/"
         Image bannerImage = new Image("file:resources/readme-banner.png");
         ImageView banner = new ImageView(bannerImage);
@@ -81,6 +109,10 @@ public class ApiApp extends Application {
 
         // setup stage
         stage.setTitle("ApiApp!");
+        stage.setMinWidth(640);
+        stage.setMinHeight(480);
+        stage.setMaxWidth(1280);
+        stage.setMaxHeight(720);
         stage.setScene(scene);
         stage.setOnCloseRequest(event -> Platform.exit());
         stage.sizeToScene();
@@ -88,4 +120,25 @@ public class ApiApp extends Application {
 
     } // start
 
+    private void getCoordinates(String city) {
+        city = URLEncoder.encode(city, StandardCharsets.UTF_8);
+        String coordQuery = String.format("city=%s", city);
+        String coordUri = "https://api.api-ninjas.com/v1/geocoding?" + coordQuery;
+
+        try {
+            // build request
+            HttpRequest request = HttpRequest.newBuilder()
+                .header("X-Api-Key", "w4JHOP4Fc54IWr0it+xLzQ==UWRhEGsLsCuvrosC")
+                .uri(URI.create(coordUri))
+                .build();    
+            
+            // send request & recieve response
+            HttpResponse<String> response = HTTP_CLIENT.send(request, BodyHandlers.ofString());
+            System.out.println(response.body());
+            //ensure request is ok
+        } catch (IllegalArgumentException | IOException | InterruptedException e) {
+            directions.setText("Last attempt to get weather failed...");
+        }
+
+    }
 } // ApiApp
